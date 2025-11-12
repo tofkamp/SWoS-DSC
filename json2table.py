c025849827019f78a8e95350ed2fd936f67e0ce0
Tjibbe Hofkamp <t_w_h@hotmail.com>
2025-11-12 12:05:13 +0100

    Add files via upload

--- json2table.py
+++ json2table.py
@@ -1,6 +1,10 @@
 import json
 
-files = [ "CRS312-4C+8XG_r2_2.18_HE208JZZ6YZ.json","CRS328-24P-4S+_r2_2.18_HJE0A2EJNXH.json"]
+files = [ "CSS326-24G-2S+__2.18_763906357EDB.json","CRS312-4C+8XG_r2_2.18_HE208JZZ6YZ.json","CRS328-24P-4S+_r2_2.18_HJE0A2EJNXH.json"]
+
+# field, api,webpage,webfield,decode
+# print to .markdown
+import csv
 
 class Table:
     def __init__(self, name):
@@ -37,19 +41,41 @@ class Table:
             print_array(row,self.rows[row])
         print("")
 
-    def print_html(self):
+    def print_html(self,fp):
         def print_array(what, array, ttype):
             line = "<tr><{ttype}>{what}</{ttype}>".format(ttype=ttype,what=what)
             for item in array:
                 line += "<{ttype}>{item}</{ttype}>".format(ttype=ttype, item=item)
-            print(line+"</tr>")
-        print("<table><caption>{about}</caption>".format(about= self.about))
+            fp.write(line+"</tr>")
+        fp.write("<table><caption>{about}</caption>".format(about= self.about))
         print_array("", self.column_headers,"th")
         for row in sorted(self.rows.keys()):
             print_array(row,self.rows[row],"td")
-        print("</table>")
-        
+        fp.write("</table>")
+
+    def write_markdown(self,fp):
+        return
+
+    def write_csv(self,csvwriter,api):
+        for row in sorted(self.rows.keys()):
+            csvwriter.writerow([row,api,"?","?","?"])
+            
 tables = {}
+
+# read the information of every field, and put it in the table
+with open('SWoS-api.csv', newline='') as csvfile:
+    csvreader = csv.DictReader(csvfile, delimiter=';', quotechar='"')
+    for row in csvreader:
+        # field;api;webpage;webfield;decode
+        if row["api"] not in tables:
+            tables[row["api"]] = Table(row["api"])     # make sure a table for this api is there
+        tables[row["api"]].add_row(row["field"])
+        for i in ("webpage","webfield","decode"):
+            #tables[row["api"]].add_row(row["webpage"])
+            tables[row["api"]].set_value(i,row["field"],row[i])
+        #tables[row["api"]].add_row(row["webfield"])
+        #tables[row["api"]].add_row(row["decode"])
+        
 for file in files:
     with open(file, 'r') as fp:
         data = json.load(fp)
@@ -73,7 +99,32 @@ for file in files:
                     if isinstance(sample_value,list):
                         tables[api].add_row(sample_key + "[]")
                         tables[api].set_value(column, sample_key + "[]", str(len(sample_value)) + "x " + sample_value[0])
-print("<html><body>")
-for table in tables:
-    tables[table].print_html()
-print("</body></html>")
+
+with open('SWoS-api-new.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csvwriter.writerow(["field","api","webpage","webfield","decode"])   # write header
+    for table in tables:
+        tables[table].write_csv(csvwriter, table)
+
+with open("SWos-api.html", mode="w") as fp:
+    fp.write("""<html><head><style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style></head><body>""")
+
+    for table in tables:
+        tables[table].print_html(fp)
+    fp.write("</body></html>")
